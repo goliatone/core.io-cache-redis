8a1305adb2904ac3f62689fbfe5a29a988adbfb6
goliatone <burgosemi@gmail.com>
2022-03-23 13:28:12 -0700

    add: export managed set

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ module.exports.init = require('./lib/init');
 module.exports.CacheClient = require('./lib/cache');
 
 module.exports.createClient = require('./lib/createClient');
+
+module.exports.ManagedSet = require('./lib/data/set');
