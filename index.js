4748213b764f3e8def315038d44be20ec3969010
goliatone <burgosemi@gmail.com>
2022-10-03 10:34:33 -0700

    add: expose error

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ module.exports.init = require('./lib/init');
 
 module.exports.CacheClient = require('./lib/cache');
 
+module.exports.CacheClientError = require('./lib/errors').CacheClientError;
+
 module.exports.createClient = require('./lib/createClient');
