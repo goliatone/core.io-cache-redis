d763e6363509f000f6ca83eb353773979d2dbd94
goliatone <burgosemi@gmail.com>
2022-10-03 19:26:22 -0700

    add: tests

--- tests/batch_test.js
+++ tests/batch_test.js
@@ -43,7 +43,7 @@ test('CacheClientBatch: "tryGetBatch" should use fallback when key not in cache'
     t.end();
 });
 
-test('CacheClientBatch: "tryGetBatch" should setBatch for keys not in cache', async t => {
+test('CacheClientBatch: "tryGetBatch" should "setBatch" for keys not in cache', async t => {
     const user1 = { user: 1, name: 'user1' };
     const key1 = '70d6e4c7-4da7-4bc9-9ecd-53e0c06a22ef';
 
@@ -76,6 +76,105 @@ test('CacheClientBatch: "tryGetBatch" should setBatch for keys not in cache', as
     t.ok(fallback.calledOnce, 'fallback should have been called once');
     t.ok(fallback.calledWith(keys), 'fallback should have been with raw key');
     t.ok(setBatch.calledWith(keys, expected), 'setBatch should have been called');
+
+    setBatch.restore();
+    await cache.client.flushall();
+
+    t.end();
+});
+
+test('CacheClientBatch: "tryGetBatch" should handle "setBatch" errors', async t => {
+    const user1 = { user: 1, name: 'user1' };
+    const key1 = '70d6e4c7-4da7-4bc9-9ecd-53e0c06a22ef';
+
+    const user2 = { user: 2, name: 'user2' };
+    const key2 = 'b6fdfba9-d8f9-40a2-a2a7-51fc34dddffc';
+
+    const user3 = { user: 3, name: 'user3' };
+    const key3 = '877fc553-0c31-49f0-b5b9-7beda30017d8';
+
+    const cache = new CacheClientBatch({
+        hashUUIDs: false,
+        logger: noopConsole(),
+        cacheKeyMatcher: CacheClientBatch.UUID_CACHE_MATCHER,
+        createClient: () => new Redis()
+    });
+
+    const keys = [key1, key2, key3];
+    const expected = [user1, user2, user3];
+
+    const fallback = sinon.stub();
+    fallback.returns(expected);
+
+    let actualError;
+    const expectedError = new Error();
+    const setBatch = sinon.stub(cache, 'setBatch');
+    setBatch.throws(expectedError);
+
+    let result;
+
+    try {
+        result = await cache.tryGetBatch(keys, fallback, {
+            throwOnError: true
+        });
+    } catch (error) {
+        actualError = error;
+    }
+
+    t.notOk(result, `result should be undefined`);
+    t.ok(fallback.calledOnce, 'fallback should have been called once');
+    t.ok(fallback.calledWith(keys), 'fallback should have been with raw key');
+    t.ok(setBatch.calledWith(keys, expected), 'setBatch should have been called');
+    t.equals(actualError, expectedError, 'should handle thrown errors');
+
+    setBatch.restore();
+    await cache.client.flushall();
+
+    t.end();
+});
+
+test('CacheClientBatch: "tryGetBatch" should return error value from "setBatch" error', async t => {
+    const user1 = { user: 1, name: 'user1' };
+    const key1 = '70d6e4c7-4da7-4bc9-9ecd-53e0c06a22ef';
+
+    const user2 = { user: 2, name: 'user2' };
+    const key2 = 'b6fdfba9-d8f9-40a2-a2a7-51fc34dddffc';
+
+    const user3 = { user: 3, name: 'user3' };
+    const key3 = '877fc553-0c31-49f0-b5b9-7beda30017d8';
+
+    const cache = new CacheClientBatch({
+        hashUUIDs: false,
+        logger: noopConsole(),
+        cacheKeyMatcher: CacheClientBatch.UUID_CACHE_MATCHER,
+        createClient: () => new Redis()
+    });
+
+    const keys = [key1, key2, key3];
+    const expected = [user1, user2, user3];
+
+    const fallback = sinon.stub();
+    fallback.returns(expected);
+
+    let actualError;
+    const expectedError = new Error();
+    const setBatch = sinon.stub(cache, 'setBatch');
+    setBatch.throws(expectedError);
+
+    let result;
+
+    try {
+        result = await cache.tryGetBatch(keys, fallback, {
+            throwOnError: false
+        });
+    } catch (error) {
+        actualError = error;
+    }
+
+    t.ok(fallback.calledOnce, 'fallback should have been called once');
+    t.ok(fallback.calledWith(keys), 'fallback should have been with raw key');
+    t.ok(setBatch.calledWith(keys, expected), 'setBatch should have been called');
+    t.equals(result.$error, expectedError, 'should handle thrown errors');
 
     setBatch.restore();
     await cache.client.flushall();
@@ -239,6 +338,82 @@ test('CacheClientBatch: "tryGetBatch" should return keys from cache', async t =>
 
     t.end();
 });
+
+test('CacheClientBatch: "getBatch" should return default values for things not in cache', async t => {
+    const user1 = { user: 1, name: 'user1' };
+    const key1 = '70d6e4c7-4da7-4bc9-9ecd-53e0c06a22ef';
+
+    const user2 = { user: 2, name: 'user2' };
+    const key2 = 'b6fdfba9-d8f9-40a2-a2a7-51fc34dddffc';
+
+    const user3 = { user: 3, name: 'user3' };
+    const key3 = '877fc553-0c31-49f0-b5b9-7beda30017d8';
+
+    const cache = new CacheClientBatch({
+        hashUUIDs: false,
+        logger: noopConsole(),
+        cacheKeyMatcher: CacheClientBatch.UUID_CACHE_MATCHER,
+        createClient: () => new Redis({
+            data: {
+                [`cache:${key1}`]: JSON.stringify(user1)
+            }
+        })
+    });
+
+    const keys = [key1, key2, key3];
+    const expected = [user1, user2, user3];
+
+    let results = await cache.getBatch(keys, [undefined, user2, user3]);
+
+    t.deepEquals(results, expected, `results match expected array`);
+
+    await cache.client.flushall();
+
+    t.end();
+});
+
+test('CacheClientBatch: "getBatch" should handle buffer data', async t => {
+    const user1 = { user: 1, name: 'user1' };
+    const key1 = '70d6e4c7-4da7-4bc9-9ecd-53e0c06a22ef';
+
+    const user2 = { user: 2, name: 'user2' };
+    const key2 = 'b6fdfba9-d8f9-40a2-a2a7-51fc34dddffc';
+
+    const user3 = { user: 3, name: 'user3' };
+    const key3 = '877fc553-0c31-49f0-b5b9-7beda30017d8';
+
+    const cache = new CacheClientBatch({
+        hashUUIDs: false,
+        logger: noopConsole(),
+        cacheKeyMatcher: CacheClientBatch.UUID_CACHE_MATCHER,
+        createClient: () => new Redis({
+            data: {
+                [`cache:${key1}`]: Buffer.from(JSON.stringify(user1)).toString('utf-8'),
+                [`cache:${key2}`]: Buffer.from(JSON.stringify(user2)).toString('utf-8'),
+                [`cache:${key3}`]: Buffer.from(JSON.stringify(user3)).toString('utf-8'),
+            }
+        })
+    });
+
+    const keys = [key1, key2, key3];
+    const expected = [user1, user2, user3];
+
+    const mgetBuffer = sinon.spy(cache.client, 'mgetBuffer');
+
+    let results = await cache.getBatch(keys, [], {
+        buffer: true,
+        deserialize(value) {
+            return JSON.parse(Buffer.from(value, 'utf-8').toString())
+        }
+    });
+
+    t.deepEquals(results, expected, `results match expected array`);
+    t.ok(mgetBuffer.calledOnce, 'should use mget buffer version');
+
+    await cache.client.flushall();
+
+    t.end();
+})
 
 test('CacheClientBatch: "setBatch" should serialize all values', async t => {
     const user1 = { user: 1, name: 'user1' };
