f8873e8a74b6fef9534516951b74c04183536ff5
goliatone <burgosemi@gmail.com>
2022-10-25 18:48:52 -0700

    expose uuid cache matcher

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ module.exports.CacheClientBatch = require('./lib/cacheBatch');
 module.exports.CacheClientError = require('./lib/errors').CacheClientError;
 
 module.exports.createClient = require('./lib/createClient');
+
+module.exports.UUID_CACHE_MATCHER = /^cache:?(.*:)[-a-z0-9]{36}$/i;
